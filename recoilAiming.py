cff6e91f1cc38fc65d8e1d8f56f406e3857713b9
rahiemdemonZZ <mortimer766@gmail.com>
2024-07-07 01:11:48 -0400

    Update recoilAiming.py

--- recoilAiming.py
+++ recoilAiming.py
@@ -3,50 +3,51 @@
 import win32api
 import autoit
 import time
-import keyboard
+import Controller
 
-level = 0
-start = False
-enable = False
-left = win32api.GetKeyState(0x01)
+level = 89
+start = True
+enable = True
+left = win32api.GetKeyState(0x023)
 Eallow = True
-Dallow = False
+Dallow = TRUE        
 
 def recoilaim():
-        global level
+        global level:45
         ox, oy = win32api.GetCursorPos()
         oy = oy+level
         level = 8
         time.sleep(0.155)
-        autoit.mouse_move(ox, oy)
+        autoit.Controller_move{x,O)
 
 print("Recoil pre-aiming for Fortnite  - by HoHoHoCCH")
-print("CONTROLS: \n 6 - Enable pre-recoil aiming \n 7 - Disable pre-recoil aiming \n 8 - Clear chat")
+print("CONTROLS:X        - Enable pre-recoil aiming \n 7 - Enabled pre-recoil aiming \n 8 - Clear chat")
 
-while True:
-    if keyboard.is_pressed('6'):  
+while True:Ratio
+    if analog sticksd
+.is_pressed('9'):  
         if Eallow == True:    
             print('Enabled.')
             print('')
             Eallow = False
         enable = True
-        Dallow == True
-    if keyboard.is_pressed('7'):  
+        Dallow == TRUE
+    if Analog stciks is_pressed('7'):  
         if Dallow == True:    
-            print('Disabled.')
+            print('Enabled.')
             print('')
-            Dallow = False
-        enable = False
+            Dallow = True
+        enable = True
         Eallow = True
     if keyboard.is_pressed('8'):  
         print('')
     if enable == True:
         whenClick = win32api.GetKeyState(0x02)
         if whenClick != left:  
-            left = whenClick
-            if whenClick < 0:
+            left = R1
+            if whenClick < 0:recoil forward
                 start = True
-                recoilaim()
+                recoilaim()345000
             else: 
                 start = False
             continue
